3d58c5ef8788f67e6e1ca9b5a818ed963ecec541
Anton Eliasson <Anton Eliasson>
2017-04-20 14:49:25 +0200

    Removed folder from repository

--- website/sleep/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class GraphDataPoints(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    yVal = models.IntegerField()
--- website/website/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import route
-from sleep.consumers import ws_message, ws_connect, ws_disconnect
-
-channel_routing = [
-    route("websocket.connect", ws_connect),
-    route("websocket.receive", ws_message),
-    route("websocket.disconnect", ws_disconnect)
-
-]
